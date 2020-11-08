d788711042b302733d8835ace8ef38691d172cf0
Antoine.D <antoine.durand@ynov.com>
2020-11-08 13:42:06 +0100

    live position

--- GPSapp/src/pages/LivePosition.tsx
+++ GPSapp/src/pages/LivePosition.tsx
@@ -1,10 +1,33 @@
-import { IonPage } from "@ionic/react"
-import React from "react"
+import { IonButton, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonPage } from "@ionic/react"
+import { save } from 'ionicons/icons'
+import { Geolocation, Geoposition } from "@ionic-native/geolocation"
+import React, { useState} from "react"
 
 const LivePosition: React.FC = () => {
+
+    const [position, setPosition] = useState<Geoposition>();
+
+    const getLocation = async() => {
+        try {
+            let position = await Geolocation.getCurrentPosition();
+            setPosition(position);
+            
+        } catch (error) {
+
+        }
+    }
+    getLocation();
+
+    let latitude = position && position.coords.latitude
+    let longitude =  position && position.coords.longitude
+
     return (
         <IonPage>
+                Your current position is:
+                Latitude:{latitude}
+                Longitude:{longitude}
             
+            <IonButton><IonIcon icon={save}/><IonLabel>Save</IonLabel></IonButton>
         </IonPage>
     )
 }
